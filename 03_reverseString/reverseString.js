c6fa29d4644ab64f8554b1b24e20fcd95248e417
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-06 15:59:37 -0400

    2 methods, one with a loop and one with split

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let result = "";
+    stringLength = inputString.length - 1; 
+    while (stringLength >= 0) {
+        result += inputString.charAt(stringLength);
+        stringLength--;
+    }
+    return result;
 
+    // return inputString.split("").reverse().join("");
 };
 
 // Do not edit below this line
