9907a67a68964bee4e7b1ce48bcd77c3315b1471
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-06 15:46:00 -0400

    declated result variable and looped to get the answer, return once the fucntion gets out of the loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+    let result = "";
+    if (num < 0) return "ERROR";
+    else {
+        while (num > 0) {
+            result += text;
+            num--;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
