9882c12c1c3c62d395b61eede0787fb01fbb949e
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-08 17:58:48 -0400

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let errorMsg = "ERROR";
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
 
+    if (a < 0 || b < 0) return errorMsg;
+
+    if (typeof a != "number" || typeof b != "number") return errorMsg;
+    
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
