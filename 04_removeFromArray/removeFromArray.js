bec0240ea0dee6cda4ef3d37dca7d03b6a43c83a
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-08 17:16:12 -0400

    used arrow function to determine if a given number is in the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const resultArray = [];
 
+    array.forEach((num) => {
+        if (!args.includes(num)) {
+            resultArray.push(num);
+        }
+    });
+
+    return resultArray;
 };
 
 // Do not edit below this line
