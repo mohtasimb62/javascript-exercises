67bc0e13f5d086213b50ea539c92651c01f46f05
Mohtasim Billah <mohtasimbillah62@gmail.com>
2023-07-30 20:25:47 -0400

    implemented all the functions in calculator.js according to the testcases in calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	if (a < b) return b - a;
+  else return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let result = 1;
+  for (let i = 0; i < args.length; i++) {
+    result *= args[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+	for (let i = a; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
